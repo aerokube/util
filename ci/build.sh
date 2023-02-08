786e81f479222f38453bb31f4e88406970a074af
Ivan Krutov <vania-pooh@aerokube.com>
2023-02-08 20:54:13 +0300

    Updated dependencies

--- ci/build.sh
+++ ci/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 export GO111MODULE="on"
-go get -u github.com/mitchellh/gox # cross compile
+go install github.com/mitchellh/gox@latest # cross compile
 gox -os "linux darwin windows" -arch "amd64" -osarch="windows/386" -output "dist/{{.Dir}}_{{.OS}}_{{.Arch}}" -ldflags "-X main.buildStamp=`date -u '+%Y-%m-%d_%I:%M:%S%p'` -X main.gitRevision=`git describe --tags || git rev-parse HEAD` -s -w"
